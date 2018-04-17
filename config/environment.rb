921bb728fae83253a07bab38c1bb21ce80f4957f
Kevin Weaver <kevinweaver2@gmail.com>
2018-04-17 14:01:22 -0400

    Add bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Load mc-settings
+Setting.load(:path  => "#{Rails.root}/config/settings",
+                         :files => ["default.yml", "environments/#{Rails.env}.yml"],
+                         :local => true)
+
 # Initialize the Rails application.
 Rails.application.initialize!
